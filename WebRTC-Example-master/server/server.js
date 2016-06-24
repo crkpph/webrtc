32fa44fdf45edb22336dc1b5fc35c7793d152a6a
crkpph <department.cr@gmail.com>
2016-06-24 11:12:11 +0800

    Update server.js

--- WebRTC-Example-master/server/server.js
+++ WebRTC-Example-master/server/server.js
@@ -1,4 +1,5 @@
-var HTTPS_PORT = 8443;
+var HTTPS_PORT = process.env.PORT;
+var IP = process.env.IP;
 
 var fs = require('fs');
 var https = require('https');
@@ -27,7 +28,7 @@ var handleRequest = function(request, response) {
 };
 
 var httpsServer = https.createServer(serverConfig, handleRequest);
-httpsServer.listen(HTTPS_PORT, '0.0.0.0');
+httpsServer.listen(HTTPS_PORT, IP);
 
 // ----------------------------------------------------------------------------------------
 
